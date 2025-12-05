72e95fc3a09729849b74049e37c9b2f0884766f1
Akshat1473 <akshat8958@gmail.com>
2025-12-05 22:28:17 +0530

    otp

--- Beginner.py
+++ Beginner.py
@@ -1,12 +1,14 @@
-# create new object for mmutable 
-x=y=z=[1,2,3]
-print(id(x),id(y),id(z))
-y[0]=8
-print(id(x),id(y),id(z))
+import random
 
-print(x,y,z)
-"""
-output:
-1760921477312 1760921477312 1760921477312
-1760921477312 1760921477312 1760921477312
-[8, 2, 3] [8, 2, 3] [8, 2, 3]"""
+otp=""
+for i in range(0,4):
+    num=random.choice(["a","1","c","3","4","5","u","7","8","9"])
+    otp+=num
+    
+print(otp)
+
+
+
+
+
+
